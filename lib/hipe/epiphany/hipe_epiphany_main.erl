3d79e0d738163764dea6a3104957ea7b802a0ebc
Magnus Lång <margnus1@telia.com>
2015-09-15 19:02:58 +0200

    Move debug code in hipe_epiphany_main to macros
    This allows selective pretty-printing when debugging (step by step)
    output is enabled.

--- lib/hipe/epiphany/hipe_epiphany_main.erl
+++ lib/hipe/epiphany/hipe_epiphany_main.erl
@@ -21,40 +21,48 @@
 -module(hipe_epiphany_main).
 -export([rtl_to_epiphany/3]).
 
+%%-define(DEBUG, 1).
+
+-ifdef(DEBUG).
+-define(IFDEBUG(X),X).
+-else.
+-define(IFDEBUG(X),ok).
+-endif.
+
 rtl_to_epiphany(MFA, RTL, Options) ->
   Defun1 = hipe_rtl_to_epiphany:translate(RTL),
-  %% io:format("~w: after translate\n", [?MODULE]),
-  %% hipe_epiphany_pp:pp(Defun1),
+  ?IFDEBUG(pp(Defun1, MFA, Options, "after translate", [])),
 
   Defun2 = hipe_epiphany_ra:ra(Defun1, Options),
-  %% io:format("~w: after regalloc\n", [?MODULE]),
-  %% hipe_epiphany_pp:pp(Defun2),
+  ?IFDEBUG(pp(Defun2, MFA, Options, "after regalloc", [])),
 
   Defun3 = hipe_epiphany_frame:frame(Defun2),
-  %% io:format("~w: after frame\n", [?MODULE]),
-  %% hipe_epiphany_pp:pp(Defun3),
+  ?IFDEBUG(pp(Defun3, MFA, Options, "after frame", [])),
 
   Defun4 = hipe_epiphany_finalise:finalise(Defun3),
-  %% io:format("~w: after finalise\n", [?MODULE]),
-  pp(Defun4, MFA, Options),
+  pp(Defun4, MFA, Options, "after finalise", []),
 
   {native, epiphany, {unprofiled, Defun4}}.
 
-pp(Defun, MFA, Options) ->
+pp(Defun, MFA, Options, _DbgFmt, _DbgArgs) ->
   case proplists:get_value(pp_native, Options) of
     true ->
+      ?IFDEBUG(io:format("~w: " ++ _DbgFmt ++ "\n", [?MODULE|_DbgArgs])),
       hipe_epiphany_pp:pp(Defun);
     {only,Lst} when is_list(Lst) ->
       case lists:member(MFA,Lst) of
 	true ->
+	  ?IFDEBUG(io:format("~w: " ++ _DbgFmt ++ "\n", [?MODULE|_DbgArgs])),
 	  hipe_epiphany_pp:pp(Defun);
 	false ->
 	  ok
       end;
     {only,MFA} ->
-       hipe_epiphany_pp:pp(Defun);
+      ?IFDEBUG(io:format("~w: " ++ _DbgFmt ++ "\n", [?MODULE|_DbgArgs])),
+      hipe_epiphany_pp:pp(Defun);
     {file,FileName} ->
       {ok, File} = file:open(FileName, [write,append]),
+      ?IFDEBUG(io:format(File, "~w: " ++ _DbgFmt ++ "\n", [?MODULE|_DbgArgs])),
       hipe_epiphany_pp:pp(File, Defun),
       ok = file:close(File);
     _ ->
